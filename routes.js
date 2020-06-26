0bb355f33cf26ba2f4984d55e890c308e4de09d6
Lucio Nonato <luciononato@hotmail.com>
2020-06-26 12:54:21 -0300

    8 commit
    INICIO DO DESAFIO 4.3; oragnização de pastas, criação de nova rota, estilização,

--- routes.js
+++ routes.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const routes = express.Router()
 
-const videos = require("./data")
-const about = require("./info")
+const videos = require('./data')
+const about = require('./info')
 
 // Rotas
-routes.get("/", function(req, res) {
-    return res.render("about", {about})
+routes.get('/', function(req, res) {
+    return res.render('about', {about})
 })
 
-routes.get("/portfolio", function(req, res) {
-    return res.render("portfolio", {items: videos})
+routes.get('/portfolio', function(req, res) {
+    return res.render('portfolio', {items: videos})
 })
 
-routes.get("/video", function(req, res) {
+routes.get('/video', function(req, res) {
     const id = req.query.id;
 
     const video = videos.find(function(video){
@@ -21,18 +21,22 @@ routes.get("/video", function(req, res) {
     })
 
     if (!video) {
-        return res.send("Video not found!")
+        return res.send('Video not found!')
     }
     
-    return res.render("video", {item: video})
+    return res.render('video', {item: video})
 })
 
-routes.get("/teachers", function(req, res) {
-    return res.render("teachers")
+routes.get('/teachers', function(req, res) {
+    return res.render('teachers/index')
 })
 
-routes.get("/students", function(req, res){
-    return res.render("students")
+routes.get('/teachers/create', function(req, res){
+    return res.render('teachers/create')
+})
+
+routes.get('/students', function(req, res){
+    return res.render('students/index')
 })
 
 module.exports = routes
