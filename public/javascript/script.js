14a1c05b0e5656d73dbada676614d62e8129810e
Lucio Nonato <luciononato@hotmail.com>
2020-07-14 20:40:56 -0300

    14.5 commit

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -8,4 +8,63 @@ for (item of menuItems) {
     }
 }
 
-console.log(currentPage)
+//Paginação
+function paginate(selectedPage, totalPages){
+    let pages = [],
+    oldPage,
+    pageAfterBefore = 2
+
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + pageAfterBefore
+        const pagesBeforeSelected = currentPage >= selectedPage - pageAfterBefore
+
+        if(firstAndLastPage || pagesBeforeSelected && pagesAfterSelectedPage) {
+            if(oldPage && currentPage - oldPage > pageAfterBefore){
+                pages.push("...")
+            }        
+            
+            if(oldPage && currentPage - oldPage == pageAfterBefore){
+                pages.push(currentPage - 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
+    }
+    return pages
+}
+
+
+function createPagination(pagination){
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+
+    let elements = ""
+
+    for(let page of pages) {
+        if(String(page).includes("...")){
+            elements += `<span>${page}</span>`
+        } else {
+            if(filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }   
+    }
+
+    pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    createPagination(pagination)
+
+}
+
--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,23 +1,35 @@
 const Teacher = require("../models/Teacher")
 
-const { age, date, graduation } = require("../../lib/utils")
+const { age, date } = require("../../lib/utils")
 
 
 
 module.exports = {
     index(req, res) {
 
-        const { filter } = req.query
+        let { filter, page, limit } = req.query
 
-        if(filter){
-            Teacher.findBy(filter, function(teachers){
-                return res.render('teachers/index', { teachers, filter })
-            })
-        } else {
-            Teacher.all(function(teachers){
-                return res.render('teachers/index', { teachers })
-            })
+        page = page || 1
+        limit = limit || 2
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(teachers){
+                const pagination = {
+                    total: Math.ceil(teachers[0].total/limit),
+                    page
+                }
+
+                return res.render('teachers/index', { teachers, pagination, filter })
+
+            }
         }
+
+        Teacher.paginate(params)
 
     },
     create(req, res){
